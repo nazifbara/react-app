6800ec22fd05854cbd40ba318a65d56ffe076b3f
zifstark <nazifbarassounon@gmail.com>
2020-09-27 17:26:15 +0100

    changed again!

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Something changed!</h1>
+        <h1>Hello!</h1>
       </header>
     </div>
   );
